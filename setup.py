101f70fe23bb4fe153d2f16595f523c3be9092d6
albertoCCz <albertocalzada95@gmail.com>
2022-07-25 17:14:01 +0200

    Compile all modified .pyx files at once

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules  = cythonize("vmd_cvm_cython/THRESHVSPFA.pyx"),
+    ext_modules  = cythonize("vmd_cvm_cython/*.pyx"),
     include_dirs = [np.get_include()]
 )
