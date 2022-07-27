901de466456f868c06ae7e86c64e585e59810f0f
albertoCCz <albertocalzada95@gmail.com>
2022-07-27 11:55:08 +0200

    Target Python 3.x version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules  = cythonize("vmd_cvm_cython/*.pyx"),
+    ext_modules  = cythonize("vmd_cvm_cython/*.pyx", language_level=3),
     include_dirs = [np.get_include()]
 )
